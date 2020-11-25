ac3b4b7567703f83f7d632e45a10ff2b136e20de
Pietro <marrazzo.pietroalfonso@gmail.com>
2020-11-25 11:56:15 +0100

    adding list

--- js/main.js
+++ js/main.js
@@ -15,12 +15,14 @@ const app = new Vue({
     // },
     methods: {
         filterGenre(newFilter) {
-            axios.get('https://developers.themoviedb.org/3', {
+            axios.get('https://developers.themoviedb.org/3/search/movie', {
                 params: {
-                    
+                    api_key: 'c97a4b0e3d4b6db1f6c5ebd74a382128',
+                    query: this.newFilter,
                 }
             })
             .then(result => {
+                console.log(result);
             })
             .catch(error => {
                 console.log(error);
