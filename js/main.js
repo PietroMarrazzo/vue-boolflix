a6e88869f3eedd6bc1e4379f07ca9786d56e3122
Pietro <marrazzo.pietroalfonso@gmail.com>
2020-11-25 12:19:18 +0100

    creating filtered array

--- js/main.js
+++ js/main.js
@@ -14,19 +14,28 @@ const app = new Vue({
     //     })
     // },
     methods: {
-        filterGenre(newFilter) {
+        filterGenre() {
+            let that = this;
             axios.get('https://developers.themoviedb.org/3/search/movie', {
                 params: {
                     api_key: 'c97a4b0e3d4b6db1f6c5ebd74a382128',
-                    query: this.newFilter,
+                    query: that.newFilter,
+                    language: 'it-IT',
                 }
             })
             .then(result => {
-                console.log(result);
+
+                let filteredList = result.data.response;
+                
+                if (this.actualGenre !== 'all') {
+                    cdFiltered = cdFiltered.filter( cd => cd.genre.toLowerCase() === this.actualGenre )
+                }
+                // console.log(result.data.result);
             })
             .catch(error => {
                 console.log(error);
-            })
+            }),
+
         }
     }
 });
