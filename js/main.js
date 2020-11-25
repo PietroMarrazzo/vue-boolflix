742f2225de7cef20c37bc8cefc5a7bd64ded29c0
Pietro <marrazzo.pietroalfonso@gmail.com>
2020-11-25 14:29:49 +0100

    adding stars

--- js/main.js
+++ js/main.js
@@ -3,38 +3,42 @@ const app = new Vue({
     data: {
         newFilter: '',
         filteredList: [],
+        vote: 0,
     },
     // created() {
-    //     axios.get('https://developers.themoviedb.org/3')
+    //     axios.get('https://api.themoviedb.org/3/search/movie?api_key=e99307154c6dfb0b4750f6603256716d&query=ritorno+al+futuro')
     //     .then( result => {
-    //         // this.cds = result.data.response;
+    //         console.log(result.data.response)
     //     })
     //     .catch(error => {
     //         console.log(error);
     //     })
     // },
     methods: {
-        filterGenre() {
-            let that = this;
-            axios.get('https://developers.themoviedb.org/3/search/movie', {
+        filter() {
+            // let that = this;
+            axios.get('https://api.themoviedb.org/3/search/movie', {
                 params: {
                     api_key: 'c97a4b0e3d4b6db1f6c5ebd74a382128',
-                    query: that.newFilter,
-                    language: 'it-IT',
+                    query: this.newFilter,
                 }
             })
             .then(result => {
 
-                let filteredList = result.data.response;
-
+                this.filteredList = result.data.results;
+                
                 // if (this.filteredList.title.includes) {
                 //     cdFiltered = cdFiltered.filter( cd => cd.genre.toLowerCase() === this.actualGenre )
                 // }
-                // console.log(result.data.result);
+                console.log(result.data.results);
             })
             .catch(error => {
                 console.log(error);
             })
+        },
+
+        getVote(vote) {
+            return Math.ceil(vote) / 2;
         }
     }
 });
