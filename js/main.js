4316e3132e04597ca0217e60089b73309639e987
Pietro <marrazzo.pietroalfonso@gmail.com>
2020-11-26 13:17:16 +0100

    css

--- js/main.js
+++ js/main.js
@@ -20,6 +20,29 @@ const app = new Vue({
             // this.results = [];
             this.filteredList = [];
             
+            // movies
+            axios.get('https://api.themoviedb.org/3/search/movie', {
+                params: {
+                    api_key: 'c97a4b0e3d4b6db1f6c5ebd74a382128',
+                    query: this.newFilter,
+                }
+            })
+            .then(result => {
+                // mergia gli array di movie e serie tv
+                const res = result.data.results;
+
+                this.filteredList = this.filteredList.concat(res);
+                console.log(filteredList);  
+
+                // const res = response.data.result;
+                // this.results = this.results.concat(res);              
+            })
+            .catch(error => {
+                console.log(error);
+            });
+
+
+            // serie TV
             axios.get('https://api.themoviedb.org/3/search/movie', {
                 params: {
                     api_key: 'c97a4b0e3d4b6db1f6c5ebd74a382128',
